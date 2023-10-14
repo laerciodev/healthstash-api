d9bc0c0240b12c07db50b5bd56a8778740f55160
Laércio <jose.laercio.bezerra@gmail.com>
2023-10-14 16:13:36 -0300

    feat: add create patient

--- src/controllers/patients.ts
+++ src/controllers/patients.ts
@@ -1,10 +1,43 @@
+import axios from 'axios';
 import { RequestHandler } from 'express';
 import { generateAccessToken } from '../helpers/generateAccessToken';
+import dotenv from 'dotenv';
 
-export const getPatients: RequestHandler = async (_, res) => {
+dotenv.config();
+
+const fhirEndpoint = process.env.FIHR_ENDPOINT as string;
+
+export const getPatients:RequestHandler = async (_, res) => {
   const accessToken = await generateAccessToken();
-  console.log(accessToken);
+  const url = fhirEndpoint + 'Patient';
+
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    'Content-type': 'application/json'
+  }
+
+  const { data } = await axios.get(url, { headers });
+
   res.json({
-    accessToken
+    patients: data.entry 
   })
 } 
+
+export const addPatient:RequestHandler = async (req, res) => {
+  const accessToken = await generateAccessToken();
+  const url = fhirEndpoint + 'Patient';
+
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    'Content-type': 'application/json'
+  }
+
+  const { data } = await axios.post(url, req.body, { headers });
+
+  res
+    .status(201)
+    .json({
+      message: 'Paciente adicionado com sucesso!',
+      patientId: data.id
+    })
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getPatients } from '../controllers/patients';
+import { getPatients, addPatient } from '../controllers/patients';
 
 const router = Router();
 
 router.get('/patients', getPatients);
+router.post('/patient', addPatient);
 
 export default router;
